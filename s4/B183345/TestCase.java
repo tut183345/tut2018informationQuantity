c9c2312d82cd19a9f2c42c7b72e74f847a2d465d
tut183345 <k183345@edu.tut.ac.jp>
2018-12-18 17:11:45 +0900

    TestCase.java:for Frequency

--- s4/B183345/TestCase.java
+++ s4/B183345/TestCase.java
@@ -4,33 +4,64 @@ import java.lang.*;
 import s4.specification.*;
 
 /*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-}
+  interface FrequencerInterface {     // This interface provides the design for frequency counter.
+  void setTarget(byte[]  target); // set the data to search.
+  void setSpace(byte[]  space);  // set the data to be searched target from.
+  int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
+  //Otherwise, it return 0, when SPACE is not set or Space's length is zero
+  //Otherwise, get the frequency of TAGET in SPACE
+  int subByteFrequency(int start, int end);
+  // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
+  // For the incorrect value of START or END, the behavior is undefined.
+  }
 */
 
 /*
-package s4.specification;
-public interface InformationEstimatorInterface{
-    void setTarget(byte target[]); // set the data for computing the information quantities
-    void setSpace(byte space[]); // set data for sample space to computer probability
-    double estimation(); // It returns 0.0 when the target is not set or Target's length is zero;
-// It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
-// The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
-// Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity, 
-}                        
+  package s4.specification;
+  public interface InformationEstimatorInterface{
+  void setTarget(byte target[]); // set the data for computing the information quantities
+  void setSpace(byte space[]); // set data for sample space to computer probability
+  double estimation(); // It returns 0.0 when the target is not set or Target's length is zero;
+  // It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
+  // The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
+  // Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
+  // Otherwise, estimation of information quantity, 
+  }                        
 */
 
 public class TestCase {
+
+    public static void testFrequency(byte[] Target, byte[] Space){
+	FrequencerInterface myObject;
+	int freq;
+	
+	myObject = new s4.B183345.Frequencer();
+	if(Target != null){
+	    myObject.setTarget(Target);
+	    System.out.print("Target:" + (new String(Target)));
+	}else{
+	    System.out.print("Target: null");
+	}
+	System.out.println("");
+
+	if(Space != null){
+	    myObject.setSpace(Space);
+	    System.out.print("Space:" + (new String(Space)));
+	}else{
+	    System.out.print("Space: null");
+	}
+	System.out.println("");
+
+	try{
+	    freq = myObject.frequency();
+	    System.out.println("freq:" + freq);
+	}catch(Exception e){
+	    System.out.println("Exception occurred: STOP");
+	}
+    }
+
     public static void main(String[] args) {
+	if(3.0/0 < 20) System.out.println("aho");
         try {
             FrequencerInterface myObject;
             int freq;
@@ -46,19 +77,12 @@ public class TestCase {
                 System.out.println("WRONG");
             }
 
-            myObject.setTarget(null);
-            freq = myObject.frequency();
-            System.out.println(">NULL" + freq);// target is not set. So output is 0.0
-            myObject.setTarget("".getBytes());
-            freq = myObject.frequency();
-            System.out.println(">" + freq);// target's length is zero. So output is 0.0
-            myObject.setTarget(String.valueOf(3 / 0.0).getBytes());
-            freq = myObject.frequency();
-            System.out.println(">infinity" + freq);// true value is infinite. So output is Double.MAX_VALUE
-            myObject.setTarget("0123".getBytes());
-            myObject.setSpace(null);
-            freq = myObject.frequency();
-            System.out.println(">0123" + freq); // space is not set. So output is Double.MAX_VALUE
+	    //Black Box Test//
+	    TestCase.testFrequency(null, "H".getBytes());// target is not set. So output is -1.0.
+	    TestCase.testFrequency("".getBytes(), "H".getBytes());// target's length is zero. So output is -1.0.
+	    TestCase.testFrequency("Hi Ho Hi Ho".getBytes(), null);// space is not set. So output is 0.0.
+	    TestCase.testFrequency("Hi Ho Hi Ho".getBytes(), "".getBytes());// space's length is zero. So output is 0.0.
+
         } catch (Exception e) {
             System.out.println("Exception occurred: STOP");
         }
@@ -81,20 +105,7 @@ public class TestCase {
             myObject.setTarget("00".getBytes());
             value = myObject.estimation();
             System.out.println(">00 " + value);
-
-            myObject.setTarget(null);
-            value = myObject.estimation();
-            System.out.println(">NULL" + value);// target is not set. So output is 0.0
-            myObject.setTarget("".getBytes());
-            value = myObject.estimation();
-            System.out.println(">" + value);// target's length is zero. So output is 0.0
-            myObject.setTarget(String.valueOf(3 / 0.0).getBytes());
-            value = myObject.estimation();
-            System.out.println(">infinity" + value);// true value is infinite. So output is Double.MAX_VALUE
-            myObject.setTarget("0123".getBytes());
-            myObject.setSpace(null);
-            value = myObject.estimation();
-            System.out.println(">0123" + value); // space is not set. So output is Double.MAX_VALUE
+	    
 
         } catch (Exception e) {
             System.out.println("Exception occurred: STOP");
@@ -102,3 +113,4 @@ public class TestCase {
 
     }
 }
+
